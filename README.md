4e30bcc7fe072dc4bbd41afc15b454a7ef59c5a6
PrashantRawatCoder <prashantrawat.coder@gmail.com>
2024-06-29 09:01:01 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>
+LISARD_TRAP
+</h1>
+A game developed using SDL2 in C++ language.
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello, World !\n";
+    return 0;
+}
